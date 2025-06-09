f6f7f36910af1b4641ec2a59dc169685d289046f
buddhsen-tripathi <buddhsen.work@gmail.com>
2025-06-09 08:57:49 +0530

    updated png to webp

--- 2.0/components/FeaturedProjects.tsx
+++ 2.0/components/FeaturedProjects.tsx
@@ -2,7 +2,7 @@
 const projects = [
   {
     title: "DeepFind.Me",
-    image: "/dfme.png",
+    image: "/dfme.webp",
     description: "Deepfind.me is an educational OSINT platform offering tools and resources to help users understand and manage their digital footprint.",
     github: null,
     demo: "https://deepfind.me?ref=buddhsen-tripathi",
@@ -10,7 +10,7 @@ const projects = [
   },
   {
     title: "Clonvo",
-    image: "/clonvo.png",
+    image: "/clonvo.webp",
     description: "Developed the website for Clonvo, an organisation which offers business solutions for AI and web development.",
     github: null,
     demo: "https://clonvo.com",
