05c84c2b79a5b2fefa869eb976209a4a59eef290
buddhsen-tripathi <buddhsen.work@gmail.com>
2025-10-19 23:39:02 -0400

    layout changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import NewsletterSubscription from '@/components/NewsletterSubscription'
 export default function Home() {
   return (
     <main className="flex-1">
-      <section className="px-6 py-12 space-y-16">
+      <section className="px-6 py-4 space-y-16">
         <Hero />
         <Skills />
         <FeaturedProjects />
